8d708f911f787bd668fe7b81b7324ebdb864bf17
RodrigoPaivaModesto <rodrigopaiva424@gmail.com>
2026-03-08 11:27:50 -0300

    Add files via upload

---
+++ #Listas (list).py
@@ -0,0 +1,16 @@
+#Listas (list)
+
+frutas = ['uva', 'maça', 'banana', 'abacaxi']
+print(frutas)
+
+marcas = []
+print(marcas)
+
+letras = list('python')
+print(letras)
+
+numeros = list(range(10))
+print(numeros)
+
+carro = ['chevrolet', 'onix', '2021', '100.000', '00km', 'prata']
+print(carro)
