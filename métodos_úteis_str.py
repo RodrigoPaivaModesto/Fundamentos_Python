470c49d055dc72728339f0a6151e701ff8025f4f
RodrigoPaivaModesto <rodrigopaiva424@gmail.com>
2026-03-08 09:56:36 -0300

    Add files via upload

---
+++ métodos_úteis_str.py
@@ -0,0 +1,32 @@
+#maiuscula, minuscula, titulo
+
+
+nome = 'roDrIgo'
+
+
+print(nome.upper())
+print(nome.lower())
+print(nome.title())
+
+
+
+#elominando espaços em branco
+
+texto = '    olá mundo       '
+
+print(texto + '.')
+print(texto.strip() + '.')
+print(texto.rstrip() + '.')
+print(texto.lstrip() + '.')
+
+
+#junção e centralização
+
+
+menu = 'girafas'
+
+print('####' + menu + '####')
+print(menu.center(20))
+print(menu.center(20, '#'))
+
+print('-'.join(menu))
