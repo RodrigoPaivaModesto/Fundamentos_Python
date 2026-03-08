ce2289715041aaf6f8bcdbbaae035ae62f5d4dad
RodrigoPaivaModesto <rodrigopaiva424@gmail.com>
2026-03-08 09:46:26 -0300

    Add files via upload

---
+++ #fatiamento de string (start, stop, step.py
@@ -0,0 +1,12 @@
+#fatiamento de string (start, stop, step)
+
+nome = 'Rodrigo Paiva Modesto'
+
+print(nome[0])
+print(nome[-1])  #pega a ultima letra da string ou a letra que voce quiser conforme o numero  inteiro negativo que voce usar
+print(nome[:7])
+print(nome[8:])
+print(nome[8:14])
+print(nome[0:19:2]) #step(ele vai pular as letras de 2 em 2 ou o numero inteiro que voce queira usar)
+print(nome[:])
+print(nome[::-1])
