6601986522c5d6b0af59dcb54810edcec66b0dff
RodrigoPaivaModesto <rodrigopaiva424@gmail.com>
2026-03-08 09:50:38 -0300

    Add files via upload

---
+++ #string de multiplas linhas ou strings t.py
@@ -0,0 +1,23 @@
+#string de multiplas linhas ou strings trilpas
+
+nome = 'Rodrigo'
+
+mensagem = f'''
+  Olá gente, me chamo {nome}.
+    Vou começar a estagiar hoje
+Espero ser de grande ajuda para vocês
+'''
+
+print(mensagem)
+
+
+print('''
+      ***************menu***************
+    
+    1 - Sacar
+    2 - Depositar
+    3 - Exibir extrato
+      
+                Obrigado por usar nosso sistema!
+      
+''')
