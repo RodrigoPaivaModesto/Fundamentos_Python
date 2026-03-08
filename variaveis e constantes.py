e9de5fde6ec1fb24e43efdc33714b93559beb69e
RodrigoPaivaModesto <rodrigopaiva424@gmail.com>
2026-03-08 09:57:41 -0300

    Add files via upload

---
+++ variaveis e constantes.py
@@ -0,0 +1,17 @@
+nome = 'rodrigo'
+idade = 25
+
+
+nome = 'Jéssica'
+
+print(nome, idade)
+
+
+limite_saque_diáio = 10000
+
+BRAZILIAN_STATES = {'SP', 'RJ', 'MG', 'SC'}
+
+print( BRAZILIAN_STATES )
+
+
+BRAZILIAN_STATES = ('são_paulo')
