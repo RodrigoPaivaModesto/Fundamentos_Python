f7e347aca3facc731ce8cc16816be80835999b3b
RodrigoPaivaModesto <rodrigopaiva424@gmail.com>
2026-03-08 09:37:31 -0300

    Add files via upload

---
+++ ##atribuição_simple.py
@@ -0,0 +1,44 @@
+##atribuição_simple
+
+saldo = 500
+print(saldo)
+
+##atribuição_com_adição
+
+saldo += 200
+print(saldo)
+
+#usando o '+' antes do '=' faz com que somem as variaveis 'saldo'
+
+
+## subtração
+
+saldo -= 200
+print(saldo)
+
+
+## multiplicação
+
+saldo *= 2
+print(saldo)
+
+## divisão e divisão inteira
+
+saldo //= 2
+print(saldo)
+
+saldo /= 1
+print(saldo)
+
+
+## exponenciação
+
+saldo **= 2
+print(saldo)
+
+
+## módulo
+
+saldo %= 25
+print(saldo)
+
